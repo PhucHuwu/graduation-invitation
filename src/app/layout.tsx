f9d9c0d279a933305a02ad21e4b608cd34ec2e41
PhucHuwu <phuctranhuu37@gmail.com>
2026-06-14 12:40:17 +0700

    fix: update title and description in metadata for graduation invitation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "../styles/index.css";
 
 export const metadata: Metadata = {
-  title: "Graduation Invitation",
-  description: "Graduation Invitation - Hoàng Thu Thủy",
+  title: "Thiệp mời tốt nghiệp - Hoàng Thu Thủy",
+  description: "Hãy cùng nhau kỷ niệm thành tựu này và chia sẻ niềm vui trong ngày đặc biệt này!",
 };
 
 export default function RootLayout({
